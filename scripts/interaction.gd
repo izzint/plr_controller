f89dba7bc0f1d3bf3be41125a5d3d65c81fcc28d
izzint <zoomer589@gmail.com>
2024-07-18 12:41:01 -0400

    remove reduntant if statement

--- scripts/interaction.gd
+++ scripts/interaction.gd
@@ -14,10 +14,9 @@ func _physics_process(_delta) -> void:
 	if Input.is_action_just_pressed("plr_interact"):
 		if not held_object:
 			release_object()
-		else:
-			if is_colliding() and get_collider() is RigidBody3D:
-				held_object = get_collider() as RigidBody3D
-				grab_object(held_object)
+		elif is_colliding() and get_collider() is RigidBody3D:
+			held_object = get_collider() as RigidBody3D
+			grab_object(held_object)
 
 func grab_object(target : RigidBody3D) -> void:
 		hold_joint.node_b = target.get_path()
