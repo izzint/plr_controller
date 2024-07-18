0e686b437a6bfc5cb923e5c512e5b315366a6e71
izzint <zoomer589@gmail.com>
2024-07-18 12:11:43 -0400

    finish hacky interactions

--- scripts/interaction.gd
+++ scripts/interaction.gd
@@ -4,9 +4,19 @@ extends RayCast3D
 Interaction Control
 '''
 
+### The joint where the interactable is held from
 @export var hold_joint : Generic6DOFJoint3D
 
-func _process(_delta):
-	if Input.is_action_just_pressed("plr_interact") and is_colliding():
-		#hold_joint.node_b = get_collider()
-		pass
+### Is the player currently holding something?
+var holding : bool = false
+
+func _physics_process(_delta) -> void:
+	if Input.is_action_just_pressed("plr_interact"):
+		if holding:
+			hold_joint.node_b = NodePath()
+			holding = false
+		else:
+			if is_colliding() and get_collider() is RigidBody3D:
+				var target = get_collider() as RigidBody3D
+				holding = true
+				hold_joint.node_b = target.get_path()
